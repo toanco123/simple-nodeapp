3aa6639a67e027e9a9886b2b145411259b209b5c
toannv <toannv@vnext.vn>
2024-10-28 11:17:43 +0700

    add update and delete countries

--- controllers/countriesController.js
+++ controllers/countriesController.js
@@ -115,6 +115,109 @@ const getPaginationCountries = async (req, res, next) => {
   }
 };
 
+const updateCountries = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (isNaN(id)) {
+      return res.status(400).json({
+        status: 400,
+        message: "ID must be a number",
+      });
+    }
+    const country = await Country.findByPk(id);
+    if (!country) {
+      return res.status(404).json({ error: "Country not found" });
+    }
+    await country.update(req.body);
+    res.status(200).json({ message: "Update successfully", data: country });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateMultipleCountries = async (req, res) => {
+    try {
+      const { ids, data } = req.body;
+  
+      if (!Array.isArray(ids) || ids.some(isNaN)) {
+        return res.status(400).json({
+          status: 400,
+          message: "IDs must be an array of numbers",
+        });
+      }
+  
+      const [updatedCount] = await Country.update(data, {
+        where: {
+          id: ids,
+        },
+      });
+  
+      if (updatedCount === 0) {
+        return res.status(404).json({ message: "No countries were updated" });
+      }
+  
+      res.status(200).json({ message: "Update successfully", updatedCount });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+const deleteCountries = async (req, res) => {
+    try {
+      const id = req.params.id;
+      if (isNaN(id)) {
+        return res.status(400).json({
+          status: 400,
+          message: "ID must be a number",
+        });
+      }
+      const country = await Country.findByPk(id);
+      if (!country) {
+        return res.status(404).json({ error: "Country not found" });
+      }
+      await country.destroy();
+      res.status(200).json({ message: "Deleted successfully", data: country });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  const deleteMultipleCountries = async (req, res) => {
+    try {
+      const { ids } = req.body;
+  
+      if (!ids || !Array.isArray(ids) || ids.length === 0) {
+        return res.status(400).json({ message: "Please provide an array of IDs to delete." });
+      }
+      const existingCountries = await Country.findAll({
+        where: { id: ids },
+        attributes: ["id"],
+      });
+
+      const existingIds = existingCountries.map(country => country.id);
+    
+      const nonExistentIds = ids.filter(id => !existingIds.includes(id));
+
+      if (nonExistentIds.length > 0) {
+        return res.status(404).json({
+          message: "Some IDs do not exist in the database.",
+          nonExistentIds,
+        });
+      }
+      
+      const deletedCount = await Country.destroy({
+        where: {
+          id: ids,
+        },
+      });
+  
+      res.status(200).json({ message: "Deleted successfully", deletedCount });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+
 const getPaginationContinentsCountriesStates = async (req, res, next) => {
   const { page = 1, limit = 10, search = "" } = req.query;
   const offset = (page - 1) * limit;
@@ -218,7 +321,7 @@ const getPaginationContinentsCountriesStates = async (req, res, next) => {
 };
 
 const getPaginationContinentsCountriesStates1 = async (req, res, next) => {
-  const { page = 1, limit = 10, search = ""} = req.query;
+  const { page = 1, limit = 10, search = "" } = req.query;
   const offset = (page - 1) * limit;
 
   try {
@@ -236,28 +339,28 @@ const getPaginationContinentsCountriesStates1 = async (req, res, next) => {
     //     ],
     //   };
     const [totalItems, countries] = await Promise.all([
-        Country.count({
+      Country.count({
+        include: [
+          {
+            model: ContinentsCountriesStates,
+            as: "continents_countries_states",
+            attributes: [],
+            required: true,
             include: [
-                {
-                  model: ContinentsCountriesStates,
-                  as: "continents_countries_states",
-                  attributes: [],
-                  required: true,
-                  include: [
-                    {
-                      model: Continents,
-                      as: "continent",
-                      // where: {
-                      //     name: {
-                      //       [Op.like]: `%${search}%`,
-                      //     },
-                      //   },
-                      attributes: [],
-                    },
-                  ],
+              {
+                model: Continents,
+                as: "continent",
+                where: {
+                  name: {
+                    [Op.like]: `%${search}%`,
+                  },
                 },
-              ],
-          }),
+                attributes: [],
+              },
+            ],
+          },
+        ],
+      }),
       Country.findAll({
         attributes: [
           "awsRegion",
@@ -291,7 +394,7 @@ const getPaginationContinentsCountriesStates1 = async (req, res, next) => {
                 //       [Op.like]: `%${search}%`,
                 //     },
                 //   },
-                attributes: [ "code", "name"],
+                attributes: ["code", "name"],
               },
             ],
           },
@@ -336,4 +439,8 @@ export {
   getPaginationCountries,
   getPaginationContinentsCountriesStates,
   getPaginationContinentsCountriesStates1,
+  updateCountries,
+  updateMultipleCountries,
+  deleteCountries,
+  deleteMultipleCountries,
 };
--- routes/countryRoutes.js
+++ routes/countryRoutes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import bodyParser from "body-parser";
-import { getCountries, getPaginationCountries, getPaginationContinentsCountriesStates1 } from "../controllers/countriesController.js";
+import { getCountries, getPaginationCountries, getPaginationContinentsCountriesStates1, updateCountries, deleteCountries, updateMultipleCountries, deleteMultipleCountries } from "../controllers/countriesController.js";
 const router = Router();
 
 router.use(bodyParser.json());
 
 router.get("/getAll",getCountries)
 router.get("/",getPaginationCountries)
+router.put("/",updateMultipleCountries)
+router.put("/:id",updateCountries)
+router.delete("/",deleteMultipleCountries)
+router.delete("/:id",deleteCountries)
 router.get("/relationship",getPaginationContinentsCountriesStates1)
 
 export default router;
